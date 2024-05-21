1b33ef7e62c0a038cdaaea40abbd3a280b732da1
Anan Moktan <ananmoktan41@gmail.com>
2024-05-21 09:56:10 -0500

    initial portfolio code

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function footer() {
+  return (
+    <footer>
+        <p>&copy; 2024 John Doe</p>
+      </footer>
+  )
+}
+
+export default footer
---
+++ src/components/references/references.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function references() {
+  return (
+    <div>references</div>
+  )
+}
+
+export default references
